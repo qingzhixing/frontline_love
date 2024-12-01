cc9b9ad0fbe733de624445ed8e7738c14eb4ab22
qingzhixing <qzsqqmail@qq.com>
2024-12-01 11:06:27 +0800

    🐜 我要玩纷争前线啊啊啊啊啊啊

---
+++ android/app/src/main/kotlin/com/example/frontline_love/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frontline_love
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+var uiTheme = ThemeData(
+  textTheme: const TextTheme(
+    headlineMedium: TextStyle(
+      fontFamily: "QingKeHuangYou-Regular",
+      fontSize: 16,
+      height: 1.5,
+      wordSpacing: 4,
+    ),
+  ),
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+  useMaterial3: true,
+);
